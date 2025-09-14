45e9b15f837c69d4811b633db85fd552165e898a
Sebastiaan <168179951+Sebastiaan335@users.noreply.github.com>
2025-09-14 13:11:54 +0200

    changed name

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { Users, BarChart3, UserPlus, Building2 } from "lucide-react";
-import Layout from "../components/ui/layout";
+import Layout from "../components/ui/ayout";
 
 interface StatCardProps {
   title: string;
